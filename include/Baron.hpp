ec6ebe7aa58f2e8c711543f1980847b5539e65ff
Uzia Peer <uzia.Peer@msmail.ariel.ac.il>
2025-06-18 20:19:33 +0300

    הוספת BARON

--- include/Baron.hpp
+++ include/Baron.hpp
@@ -1,1 +1,16 @@
 // peeruzia@gmail.com
+#pragma once
+#include "Player.hpp"
+
+namespace coup {
+
+class Baron : public Player {
+public:
+    Baron(Game& game, const std::string& name);
+
+    void tax() override;       // כמו player רגיל
+    void invest();             // הפעולה הייחודית
+    void onSanction();         // קוראים לה אם נפגע מ-sanction
+};
+
+}
--- src/Baron.cpp
+++ src/Baron.cpp
@@ -1,1 +1,32 @@
 // peeruzia@gmail.com
+#include "Baron.hpp"
+#include <stdexcept>
+
+using namespace std;
+
+namespace coup {
+
+Baron::Baron(Game& game, const string& name) : Player(game, name) {}
+
+void Baron::tax() {
+    if (!active) throw runtime_error("Baron is not active.");
+    if (game.turn() != name) throw runtime_error("Not your turn.");
+    coin_count += 2;
+    game.nextTurn();
+}
+
+void Baron::invest() {
+    if (!active) throw runtime_error("Baron is not active.");
+    if (game.turn() != name) throw runtime_error("Not your turn.");
+    if (coin_count < 3) throw runtime_error("Not enough coins to invest.");
+
+    coin_count -= 3;
+    coin_count += 6;
+    game.nextTurn();
+}
+
+void Baron::onSanction() {
+    coin_count += 1;  // מקבל פיצוי
+}
+
+}
