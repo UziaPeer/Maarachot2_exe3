c783c9ae2f3741510c2a129bfae015b8495709cb
Uzia Peer <uzia.Peer@msmail.ariel.ac.il>
2025-06-18 20:16:24 +0300

    הוספת SPY

--- include/Spy.hpp
+++ include/Spy.hpp
@@ -1,1 +1,22 @@
 // peeruzia@gmail.com
+#pragma once
+#include "Player.hpp"
+#include <unordered_map>
+
+namespace coup {
+
+class Spy : public Player {
+private:
+    std::unordered_map<std::string, bool> arrest_blocked;
+
+public:
+    Spy(Game& game, const std::string& name);
+
+    int reveal(Player& target) const;     // לראות כמה מטבעות יש לו
+    void block_arrest(Player& target);    // לחסום את ה-arrest שלו
+
+    bool isArrestBlocked(const std::string& player_name) const;
+    void clearBlock(const std::string& player_name); // לקרוא לזה בתחילת התור של השחקן ההוא
+};
+
+}
--- src/Spy.cpp
+++ src/Spy.cpp
@@ -1,1 +1,30 @@
 // peeruzia@gmail.com
+#include "Spy.hpp"
+#include <stdexcept>
+
+using namespace std;
+
+namespace coup {
+
+Spy::Spy(Game& game, const string& name) : Player(game, name) {}
+
+int Spy::reveal(Player& target) const {
+    if (!target.isActive()) throw runtime_error("Target not active.");
+    return target.coins();
+}
+
+void Spy::block_arrest(Player& target) {
+    if (!target.isActive()) throw runtime_error("Target not active.");
+    arrest_blocked[target.getName()] = true;
+}
+
+bool Spy::isArrestBlocked(const string& player_name) const {
+    auto it = arrest_blocked.find(player_name);
+    return it != arrest_blocked.end() && it->second;
+}
+
+void Spy::clearBlock(const string& player_name) {
+    arrest_blocked.erase(player_name);
+}
+
+}
